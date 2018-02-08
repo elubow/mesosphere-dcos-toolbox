a0270bbf156724d301ab19f163364ce23e2eda7e
Eric Lubow <eric@lubow.org>
2018-02-08 15:55:43 +0000

    create_keytabs_for_ad_kafka_zk: initial import

--- scripts/README.md
+++ scripts/README.md
@@ -1,6 +1,9 @@
 # Scripts
 This is a list of scripts that have been put together for one reason or another.
 
+## create_keytabs_for_ad_kafka_zk.sh
+Creates a keytab from an Active Directory Kerberos server using utilities on a Linux machine. It is expected that the Linux server can access Kerberos utilities and the Active Directory server.
+
 ## deploy_confluent_platform.sh
 Launches a single zookeeper with multiple Confluent Kafka platforms. The Confluent Kafka platform includes the Schema Registry, Connect, Rest Proxy, Replicator and core Kafka. It will also install the Confluent Control Center (C3) separately.
 
---
+++ scripts/create_keytabs_for_ad_kafka_zk.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# This file:
+#
+# - Creates a keytab file on linux that has access to a Windows AD master
+
+# Constants
+ENCRYPTION_TYPES=("arcfour-hmac-md5" "des-cbc-crc" "des-cbc-md5" "arcfour-hmac")
+PASSWORD="DeleteM3!"
+
+# Zookeeper
+echo -n "Enter Zookeeper keytab filename: "
+read ZK_KEYTAB
+
+if [[ -f ${ZK_KEYTAB} ]]
+then
+        rm -f ${ZK_KEYTAB}
+fi
+
+kvno=1
+for instance_no in 1 2 3
+do
+   for etype in ${ENCRYPTION_TYPES[@]}
+   do
+        printf "%b" "addent -password -p kafka/kafka-${instance_no}-broker.beta-confluent-kafka.autoip.dcos.thisdcos.directory -k $kvno -e ${etype}\n${PASSWORD}\nwrite_kt ${ZK_KEYTAB}" |ktutil
+    ((kvno++))
+   done
+done
+
+echo "Created Zookeeper keytab file: ${ZK_KEYTAB}"
+base64 -w 0 ${ZK_KEYTAB} > ${ZK_KEYTAB}.base64
+echo "Created base64 encoded ZK keytab: ${ZK_KEYTAB}.base64"
+
+
+# Kafka
+echo -n "Enter Kafka keytab filename: "
+read KAFKA_KEYTAB
+
+if [[ -f ${KAFKA_KEYTAB} ]]
+then
+        rm -f ${KAFKA_KEYTAB}
+fi
+
+kvno=1
+for instance_no in 1 2 3
+do
+   for etype in ${ENCRYPTION_TYPES[@]}
+   do
+        printf "%b" "addent -password -p kafka/kafka-${instance_no}-broker.beta-confluent-kafka.autoip.dcos.thisdcos.directory -k $kvno -e ${etype}\n${PASSWORD}\nwrite_kt ${KAFKA_KEYTAB}" |ktutil
+    ((kvno++))
+   done
+done
+
+echo "Created Kafka keytab file: ${KAFKA_KEYTAB}"
+base64 -w 0 ${KAFKA_KEYTAB} > ${KAFKA_KEYTAB}.base64
+echo "Created base64 encoded KAFKA keytab: ${KAFKA_KEYTAB}.base64"
+
